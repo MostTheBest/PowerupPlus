17f9f38d5f9a091f12fce47ef714dff976970cf7
Lodingbot <alimohamad0409@gmail.com>
2016-06-12 16:44:02 +0000

    Update Stats.lua

--- plugins/Stats.lua
+++ plugins/Stats.lua
@@ -72,7 +72,6 @@ local function bot_stats()
   local redis_scan = [[
     local cursor = '0'
     local count = 0
-
     repeat
     local r = redis.call("SCAN", cursor, "MATCH", KEYS[1])
     cursor = r[1]
@@ -91,10 +90,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'signal' then -- Put everything you like :)
+  if matches[1]:lower() == 'powerup' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /signal ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /powerup ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "signal" then -- Put everything you like :)
+    if matches[2] == "powerup" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +142,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (signal)",
-	"^[#!/]([Ss]ignal)"
+    "^[#!/]([Ss]tats) (powerup)",
+	"^[#!/]([Pp]owerup)"
     }, 
   run = run
 }
