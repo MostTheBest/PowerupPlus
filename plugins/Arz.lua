c0eb7fa40b6fea2eb6409db00b9efac0c43da0ab
Lodingbot <alimohamad0409@gmail.com>
2016-06-18 17:34:26 +0000

    Update Arz.lua

--- plugins/Arz.lua
+++ plugins/Arz.lua
@@ -2,8 +2,7 @@ local function get_arz()
   local url = 'http://exchange.nalbandan.com/api.php?action=json'
   local jstr, res = http.request(url)
   local arz = json:decode(jstr)
-  return '📊 نرخ ارز ، طلا و سکه در:'..arz.dollar.date..'\n\n〽️ هر گرم طلای 18 عیار:'..arz.gold_per_geram.value..' تومان\n\n🌟 سکه طرح جدید:'..arz.coin_new.value..' تومان\n\n⭐️ سکه طرح قدیم:'..arz.coin_old.value..' تومان\n\n💵 دلار آمریکا:'..arz.dollar.value..' تومان\n\n💵 دلـار رسمی:'..arz.dollar_rasmi.value..' تومان\n\n💶 یورو:'..arz.euro.value..' تومان\n\n💷 پوند:'..arz.pond.value..' تومان\n\n💰 درهم:'..arz.derham.value..' تومان'
-end
+  return 'نرخ ارز سکه...\n⭐️ سکه طرح قدیم:'..arz.coin_old.value..' تومان\\n سکه طرح جدید:'..arz.coin_new.value..' تومان\'..\n➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖\nنرخ ارز پول...\n دلـار رسمی:'..arz.dollar_rasmi.value..' تومان\\n دلار آمریکا:'..arz.dollar.value\تومان '..\n یورو:'..arz.euro.value\تومان '..\n پوند:'..arz.pond.value..' تومان\\n درهم:'..arz.derham.value..' تومان'\n➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖ ➖\nنرخ ارز طلا...\n نرخ ارز ، طلا و سکه در:'..arz.dollar.date..'\n\n〽️ هر گرم طلای 18 عیار:'..arz.gold_per_geram.value..' تومان
 
 local function run(msg, matches)
   local text
