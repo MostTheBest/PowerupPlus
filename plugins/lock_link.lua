e0d7c45414dd75f33c2605e892ec96b9215afaa6
Lodingbot <alimohamad0409@gmail.com>
2016-06-22 02:00:22 +0430

    Update lock_link.lua

--- plugins/lock_link.lua
+++ plugins/lock_link.lua
@@ -1,28 +1,42 @@
-local function run(msg)
-    
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-    
-     if data[tostring(msg.to.id)]['settings']['lock_link'] == '🔒' then
-      
-    
-if not is_momod(msg) then
-    
-    
-chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msgads = 'ForbiddenAdText'
-   local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
-	
-      end
-   end
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_link'] then
+                lock_english = data[tostring(msg.to.id)]['settings']['lock_link']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_link == "🔒" then
+       delete_msg(msg.id, ok_cb, true)
+    end
 end
-    
-return {patterns = {
+ 
+return {
+  patterns = {
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
 "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
 "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
 "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
 "[Hh][Tt][Tt][Pp]://",
 "[Ww][Ww][Ww]:",
-}, run = run}
---Thanks To Creed
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/(.*)",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt](.*)",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/(.*)",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/(.*)",
+"[Hh][Tt][Tt][Pp]://(.*)",
+"(.*)[Ww][Ww][Ww]:",
+"(.*)[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"(.*)[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+"(.*)[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"(.*)[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"(.*)[Hh][Tt][Tt][Pp]://",
+"(.*)[Ww][Ww][Ww]:"
+ },
+  run = run
+}
