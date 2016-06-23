e791511fccf2ad9aef39c646710dc254dc579c6c
Lodingbot <alimohamad0409@gmail.com>
2016-06-23 09:12:39 +0000

    Update Lock_Links.lua

--- plugins/Lock_Links.lua
+++ plugins/Lock_Links.lua
@@ -14,6 +14,7 @@ local function run(msg, matches)
     local user = "user#id"..msg.from.id
     if lock_link == "🔒" then
        delete_msg(msg.id, ok_cb, true)
+       return "🗣FUCK YOU👌"
     end
 end
 
