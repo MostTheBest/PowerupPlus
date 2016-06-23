b913b463df579ca84bb6ef88e7be6924df73a6b1
Lodingbot <alimohamad0409@gmail.com>
2016-06-23 10:24:30 +0000

    Update TextSticker.lua

--- plugins/TextSticker.lua
+++ plugins/TextSticker.lua
@@ -38,6 +38,7 @@ local url = 'http://latex.codecogs.com/png.latex?'..'\\dpi{'..size..'}%20\\huge%
 local file = download_to_file(url,'file.webp')
 if msg.to.type == 'channel' then
 send_document('channel#id'..msg.to.id,file,ok_cb,false)
+return "🗣PLEASE WAIT👇🏻"
 end
 end
 return {
