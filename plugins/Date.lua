e0699c0565d2dcc247f06363d3cfab9507112600
Lodingbot <alimohamad0409@gmail.com>
2016-07-02 21:56:57 +0000

    Update Date.lua

--- plugins/Date.lua
+++ plugins/Date.lua
@@ -1,5 +1,6 @@
 do
 function run(msg, matches)
+send_document(get_receiver(msg),'file/images.webp',ok_cb,false)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
@@ -14,3 +15,4 @@ return {
 run = run 
 } 
 end
+--Thanks To 
